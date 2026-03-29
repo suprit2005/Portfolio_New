8f5593e5d41afa1dc28798cce1811dd2d3cfc207
{suprit2005} <{supritmahajan@gmail.com}>
2026-03-30 01:35:59 +0530

    deploy setting

--- portfolio_site/portfolio_site/settings.py
+++ portfolio_site/portfolio_site/settings.py
@@ -75,7 +75,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio_site.wsgi.application'
 
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 # Database
 DATABASES = {
     'default': {
@@ -107,8 +111,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Profile images, project images, certificates)
